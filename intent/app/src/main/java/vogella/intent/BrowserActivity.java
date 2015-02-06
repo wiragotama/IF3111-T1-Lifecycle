a1e913a6dbc06ceac446b05a8d00d96e7366f9a1
wiragotama <wiragotama@gmail.com>
2015-02-06 16:10:17 +0700

    second

---
+++ intent/app/src/main/java/vogella/intent/BrowserActivity.java
@@ -0,0 +1,52 @@
+package vogella.intent;
+
+/**
+ * Created by wira gotama on 2/6/2015.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class BrowserActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // To keep this example simple, we allow network access
+        // in the user interface thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        TextView text = (TextView) findViewById(R.id.textView);
+        // To get the action of the intent use
+        String action = intent.getAction();
+        if (!action.equals(Intent.ACTION_VIEW)) {
+            throw new RuntimeException("Should not happen");
+        }
+        // To get the data use
+        Uri data = intent.getData();
+        URL url;
+        try {
+            url = new URL(data.getScheme(), data.getHost(), data.getPath());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                text.append(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ intent/app/src/main/java/vogella/intent/ResultActivity.java
@@ -0,0 +1,33 @@
+package vogella.intent;
+
+/**
+ * Created by wira gotama on 2/6/2015.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+        Bundle extras = getIntent().getExtras();
+        String inputString = extras.getString("yourkey");
+        TextView view = (TextView) findViewById(R.id.displayintentextra);
+        view.setText(inputString);
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        EditText editText= (EditText) findViewById(R.id.returnValue);
+        String string = editText.getText().toString();
+        intent.putExtra("returnkey", string);
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+}
